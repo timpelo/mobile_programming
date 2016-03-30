186dbb807c3f6af185fdca47939660ca04248361
Jani <janij.timonen@gmail.com>
2016-03-30 11:21:50 +0300

    Created game activity.

--- Mobile/RPSApp/app/src/main/java/com/tiko/tamk/rpsapp/MainActivity.java
+++ Mobile/RPSApp/app/src/main/java/com/tiko/tamk/rpsapp/MainActivity.java
@@ -1,7 +1,15 @@
 package com.tiko.tamk.rpsapp;
 
+import android.content.Intent;
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +17,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView title = (TextView) findViewById(R.id.title);
+        Button playButton = (Button) findViewById(R.id.playButton);
+        ImageView titleBg = (ImageView) findViewById(R.id.titleBg);
+
+        Animation fade =
+                AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fadein);
+        Animation pulse =
+                AnimationUtils.loadAnimation(getApplicationContext(), R.anim.pulse);
+
+
+        title.startAnimation(fade);
+        playButton.startAnimation(fade);
+        titleBg.startAnimation(pulse);
+    }
+
+    public void startGame(View view) {
+        Intent intent = new Intent(this, GameActivity.class);
+        startActivity(intent);
     }
 }
-l
