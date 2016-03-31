bf09fc1d23a2beb658207e85513aa15eb110c298
Jani <janij.timonen@gmail.com>
2016-03-31 12:38:31 +0300

    Created map for location

--- Mobile/MyRunApp/app/src/main/java/com/tiko/tamk/myrunapp/MainActivity.java
+++ Mobile/MyRunApp/app/src/main/java/com/tiko/tamk/myrunapp/MainActivity.java
@@ -1,27 +1,61 @@
 package com.tiko.tamk.myrunapp;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private Location latestLocation;
+    private MapFragment mapFragment;
+    final private String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        MapFragment mapFragment = (MapFragment) getFragmentManager()
+        // Finds map fragment.
+        mapFragment = (MapFragment) getFragmentManager()
                 .findFragmentById(R.id.map);
 
         mapFragment.getMapAsync(this);
+
+
+
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
 
+        // Checks permission and gets latests location from gps provider.
+        if (ActivityCompat.checkSelfPermission(
+                this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+
+            LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+            latestLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        }
+
+        // If latest location is stored, move marker to latest location.
+        double longitude = latestLocation.getLongitude();
+        double latitude = latestLocation.getLatitude();
+
+        Log.d(TAG, longitude + ", " + latitude);
+
+        LatLng latLng = new LatLng(latitude, longitude);
+        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15f));
     }
 }
